c28db023477fa961124b2fafb6d000e6a51e4c0e
gekicchi <oyarzunm18@gmail.com>
2025-07-10 01:29:32 -0400

    create quiz pages

---
+++ lib/pages/island_quiz_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IslandQuiz extends StatefulWidget {
+  const IslandQuiz({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _IslandQuizState();
+}
+
+class _IslandQuizState extends State<IslandQuiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/pages/personality_quiz_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalityQuiz extends StatefulWidget {
+  const PersonalityQuiz({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PersonalityQuizState();
+}
+
+class _PersonalityQuizState extends State<PersonalityQuiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
